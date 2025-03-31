80664b3670dcfe72464ff213270f187327a44307
dhxogud <hyong0117@naver.com>
2025-03-31 16:43:14 +0900

    수업
    20250331

---
+++ Source1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Number1 = 1000;
+	int Number2 = 1000;
+
+
+	float A = (float)5;
+	int B = (int)A;
+
+
+	cout << A << endl;
+
+	return 0;
+}
