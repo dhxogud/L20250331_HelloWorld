15193bc0253833bf669332235d90c5e081c48454
dhxogud <hyong0117@naver.com>
2025-04-01 09:58:27 +0900

    테스트

--- Source1.cpp
+++ Source1.cpp
@@ -10,7 +10,8 @@ int main()
 	float Float1 = 4.4f;
 	float Float2 = 6.6f;
 
-	cout << Number1 + Number2 << endl;
+	cout << (Number1 + Number2);
+	cout.flush();
 	cout << Number1 - Number2 << endl;
 	cout << Number1 * Number2 << endl;
 	cout << Number1 / Number2 << endl;
